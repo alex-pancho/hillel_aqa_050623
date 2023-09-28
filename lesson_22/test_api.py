8a6191108c46e664d1ce5a5abb6a2d31f1c6f4f0
OPanchenko <pan.pan.4it@gmail.com>
2023-09-28 19:40:05 +0300

    Test 1

--- lesson_22/test_api.py
+++ lesson_22/test_api.py
@@ -1,8 +1,6 @@
-import requests
 import pytest
-from hillel_api import s, auth
-
-
+from hillel_api import s, auth, cars, expenses, users
+"""
 def test_signup_positive():
     user_data= {
         "name": "John",
@@ -29,7 +27,7 @@ def test_signin_positive():
     assert r_json.get('status', False) == "ok", "Key 'status' is not ok"
 
 def get_a(a):
-    """Nazva testu"""
+    
     return int(a)
 
 def test_error():
@@ -52,7 +50,7 @@ def test_error_message():
         get_error()
 
 # Homework
-"""
+
 https://qauto.forstudy.space/api-docs/
 Написати п'ять тестів що проходять через пункти
 1. Реєстрація користувача
@@ -62,3 +60,84 @@ https://qauto.forstudy.space/api-docs/
 5. Видалення користувача
 """
 
+
+def test_auth_signup_positive():
+    """
+    Test of API functionality of user signup
+    """
+    user_data = {
+        "name": "John",
+        "lastName": "Dou",
+        "email": "1122334455@test.com",
+        "password": "Qam2608venv",
+        "repeatPassword": "Qam2608venv"
+    }
+    r = auth.signup(s, user_data)
+    r_json = r.json()
+    assert r.status_code == 201, f"Wrong status code:\n{r_json}"
+    assert r_json.get('status', False) == "ok", "Key 'status' is not ok"
+
+
+car_id = None
+
+
+def test_cars_post_positive():
+    """
+    Test of API functionality of car creation
+    """
+    cars_data = {
+        "carBrandId": 1,
+        "carModelId": 1,
+        "mileage": 1222
+    }
+    global car_id
+    r = cars.cars_post(s, cars_data)
+    r_json = r.json()
+    car_id = r_json.get('data', {}).get('id')
+    assert r.status_code == 201, f"Wrong status code:\n{r_json}"
+    assert r_json.get('status', False) == "ok", "Key 'status' is not ok"
+
+
+def test_cars_put_positive():
+    """
+    Test of API functionality of updating car information
+    """
+    global car_id
+    if car_id is None:
+        pytest.fail("No car_id available. Please run test_cars_post_positive first.")
+
+    updated_cars_data = {
+        "id": car_id,
+        "carBrandId": 1,
+        "carModelId": 1,
+        "mileage": 168223,
+    }
+    r = cars.cars_id_put(s, updated_cars_data)
+    r_json = r.json()
+    assert r.status_code == 200, f"Wrong status code:\n{r_json}"
+    assert r_json.get('status', False) == "ok", "Key 'status' is not ok"
+
+
+def test_expenses_get_positive():
+    """
+    Test of API functionality of receiving car's data
+    """
+    car_id = test_cars_put_positive()
+    request_body = {
+        "id": car_id,
+        "page": 0
+    }
+    r = expenses.expenses_get(s, request_body)
+    r_json = r.json()
+    assert r.status_code == 200, f"Wrong status code:\n{r_json}"
+    assert r_json.get('status', False) == "ok", "Key 'status' is not ok"
+
+
+def test_user_delete_positive():
+    """
+    Test of API functionality of deleting user's data
+    """
+    r = users.users(s)
+    r_json = r.json()
+    assert r.status_code == 200, f"Wrong status code:\n{r_json}"
+    assert r_json.get('status', False) == "ok", "Key 'status' is not ok"
